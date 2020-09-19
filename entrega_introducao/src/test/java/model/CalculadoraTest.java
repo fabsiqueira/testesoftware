9a8d95f162e0ac67b8a0a6ea94191cc5dd8d2a67
fabsiqueira <fabssiqueira@hotmail.com>
2020-09-18 23:01:26 -0300

    Teste de Sucesso

--- entrega_introducao/src/test/java/model/CalculadoraTest.java
+++ entrega_introducao/src/test/java/model/CalculadoraTest.java
@@ -10,51 +10,51 @@ public class CalculadoraTest{
 	public void somarTest(){
 		Calculadora soma = new Calculadora();
 		int resultado = soma.somar(3,4);
-		assertEquals(70, resultado);
+		assertEquals(7, resultado);
 	}
 	@Test 
 	public void somarTestNegativo(){
 		Calculadora soma = new Calculadora();
 		int resultado = soma.somar(-2,-8);
-		assertEquals(-100, resultado);
+		assertEquals(-10, resultado);
 	}
 		
 	@Test 
 	public void subtrairTest(){
 		Calculadora sub = new Calculadora();
 		int resultado = sub.subtrair(3,1);
-		assertEquals(20, resultado);
+		assertEquals(2, resultado);
 	}
 	@Test 
 	public void subtrairTestNegativo(){
 		Calculadora sub = new Calculadora();
 		int resultado = sub.subtrair(-8,4);
-		assertEquals(-120, resultado);
+		assertEquals(-12, resultado);
 	}
 	
 	@Test 
 	public void multiplicarTest(){
 		Calculadora mult = new Calculadora();
 		int resultado = mult.multiplicar(5,4);
-		assertEquals(200, resultado);
+		assertEquals(20, resultado);
 	}
 	@Test 
 	public void multiplicarTestZero(){
 		Calculadora mult = new Calculadora();
 		int resultado = mult.multiplicar(10,0);
-		assertEquals(90, resultado);
+		assertEquals(0, resultado);
 	}
 	@Test 
 	public void dividirTest(){
 		Calculadora div = new Calculadora();
 		int resultado = div.dividir(20,4);
-		assertEquals(50, resultado);
+		assertEquals(5, resultado);
 	}
 	@Test 
 	public void dividirTestNegativo(){
 		Calculadora div = new Calculadora();
 		int resultado = div.dividir(20,-5);
-		assertEquals(-40, resultado);
+		assertEquals(-4, resultado);
 	}
 	
 }
